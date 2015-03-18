8190a0488629e9379a7d6cd0d2297a2ffec9959c
alhart2015 <alhart@davidson.edu>
2015-03-18 11:29:35 -0400

    I'm going to murder MATLAB. Bugs on bugs on bugs.

--- WebTree_LP.m
+++ WebTree_LP.m
@@ -11,7 +11,7 @@ FILENAME = 'WebTree Data/fall-2013-edited.csv'; % You need to edit the file
                                                 % to remove the column
                                                 % headers.
 fid = fopen(FILENAME);
-data = textscan(fid, '%d %s %d %d %d %d %s %s %s %s %s', 'delimiter', ',');
+data = textscan(fid, '%s %s %s %s %s %s %s %s %s %s %s', 'delimiter', ',');
 fclose(fid);
 
 ID = data{1};
@@ -25,6 +25,35 @@ major2 = data{8};
 subject = data{9};
 number = data{10};
 seq = data{11};
+
+all_data = [ID class_year crn tree branch course_celing];
+
+sr_string = cellstr('SENI');
+sr_replace = cellstr('4');
+jr_string = cellstr('JUNI');
+jr_replace = cellstr('3');
+so_string = cellstr('SOPH');
+so_replace = cellstr('2');
+fr_string = cellstr('FRST');
+fr_replace = cellstr('1');
+ot_replace = cellstr('0');
+
+for i = 1:length(class_year)        % Replace the spellings of the class year
+    current_year = class_year(i);   % with something you can sort by
+    if strcmp(current_year, sr_string)
+        class_year(i) = sr_replace;
+    elseif strcmp(current_year, jr_string)
+        class_year(i) = jr_replace;
+    elseif strcmp(current_year, so_string)
+        class_year(i) = so_replace;
+    elseif strcmp(current_year, fr_string)
+        class_year(i) = fr_replace;
+    else
+        class_year(i) = ot_replace;
+    end
+end
+
+sorted_data = sortrows(all_data, [-2 1]);   % Sort by year then ID number
 
 people = unique(ID);
 courses = unique(crn);
--- get_student_pref.m
+++ get_student_pref.m
@@ -20,6 +20,6 @@ function [student_pref_matrix] = get_student_pref(num_unique_students, possibles
     for i = 1:length(student_ids)   % Do this for every row in the table
         student_in_question = student_ids(i);   % Student you're looking at
         class_in_question = crns(i);            % Corresponding class
-        preference = 7*(trees(i) - 1) + branches(i);    % How bad they want it
+        preference = 7*(typecast(trees(i), 'int32') - 1) + typecast(branches(i), 'int32');    % How bad they want it
         student_pref_matrix(student_in_question, preference) = class_in_question;
     end
